c12f5545a25f01405d9799dbf30c7238f134b5e0
Tomaz Muraus <tomaz@tomaz.me>
2017-06-26 11:44:24 +0200

    Add parse_file action to csv pack.

---
+++ actions/parse_file.py
@@ -0,0 +1,27 @@
+import os
+import csv
+from StringIO import StringIO
+
+from st2actions.runners.pythonrunner import Action
+
+__all__ = [
+    'ParseCSVFileAction'
+]
+
+
+class ParseCSVFileAction(Action):
+    def run(self, file_path, delimiter=',', quote_char='"'):
+        if not os.path.isfile(file_path):
+            raise ValueError('File "%s" doesnt exist')
+
+        with open(file_path, 'r') as fp:
+            content = fp.read()
+
+        fh = StringIO(content)
+
+        reader = csv.reader(fh, delimiter=str(delimiter), quotechar=str(quote_char))
+
+        result = []
+        for row in reader:
+            result.append(row)
+        return result
