96971a3d9206db9302136d9edfe3e98d97627ebc
Tomaz Muraus <tomaz@tomaz.me>
2017-06-26 11:52:34 +0200

    Use open file handle directly.

--- actions/parse_file.py
+++ actions/parse_file.py
@@ -1,6 +1,5 @@
 import os
 import csv
-from StringIO import StringIO
 
 from st2actions.runners.pythonrunner import Action
 
@@ -14,14 +13,11 @@ class ParseCSVFileAction(Action):
         if not os.path.isfile(file_path):
             raise ValueError('File "%s" doesnt exist' % (file_path))
 
-        with open(file_path, 'r') as fp:
-            content = fp.read()
-
-        fh = StringIO(content)
-
-        reader = csv.reader(fh, delimiter=str(delimiter), quotechar=str(quote_char))
-
         result = []
-        for row in reader:
-            result.append(row)
+        with open(file_path, 'r') as fh:
+            reader = csv.reader(fh, delimiter=str(delimiter), quotechar=str(quote_char))
+
+            for row in reader:
+                result.append(row)
+
         return result
